499a7e9387cd2b4962700f1a3e9045cca2f7830b
Baz86 <thomasebale@gmail.com>
2019-10-08 18:30:06 +0100

    ran migration for news articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190502170737) do
+ActiveRecord::Schema.define(version: 20191008171607) do
+
+  create_table "news_items", force: :cascade do |t|
+    t.text "body"
+    t.integer "player_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
