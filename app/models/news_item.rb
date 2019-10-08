205ad190363709b67a4884fa6c898be816b59ccf
Baz86 <thomasebale@gmail.com>
2019-10-08 18:36:23 +0100

    set up association between news_items and player

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,2 +1,7 @@
 class NewsItem < ApplicationRecord
+
+    belongs_to :player
+
+    validate_presence_of :player_id
+    
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ApplicationRecord
 
     has_many :weekly_scores
+    has_many :news_items, dependent: :destroy
 
     validates_presence_of :name
     validates_presence_of :team
